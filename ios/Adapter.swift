822015160c817fee0d705694b28f5bb403217b8f
Georgy Mishin <geomishin@gmail.com>
2022-12-02 12:42:19 +0300

    Initial commit

---
+++ ios/Adapter.swift
@@ -0,0 +1,8 @@
+//
+//  Adapter.swift
+//  ListsExamples
+//
+//  Created by Георгий Мишин on 02.12.2022.
+//
+
+import Foundation
---
+++ ios/NativeList.swift
@@ -0,0 +1,8 @@
+//
+//  NativeList.swift
+//  ListsExamples
+//
+//  Created by Георгий Мишин on 02.12.2022.
+//
+
+import Foundation
